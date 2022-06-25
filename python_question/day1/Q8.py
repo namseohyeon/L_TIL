ba522e4ee312450b6222e465c6695d23f62efdc8
namseohyeon <syn5421a@gamil.com>
2022-06-26 00:54:28 +0900

    Q6, Q7 풀이

---
+++ python_question/day1/Q8.py
@@ -0,0 +1,13 @@
+# for i in range(0,60,3):
+#     print(i, end="")
+    # for j in range(0,i):
+    #     print(i*j)
+
+twolist = [4][5]
+for i in range (0, 4):
+    print(i)
+    for j in range (0,5):
+        print(j)
+        x = 0
+        twolist[i][j].append(3)
+        print(twolist, end=" ")
