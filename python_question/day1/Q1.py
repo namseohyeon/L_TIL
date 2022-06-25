0d98f36f75eb36276bf299a19c6e2f9650649fab
namseohyeon <syn5421a@gamil.com>
2022-06-26 00:17:32 +0900

    2.1 ~ 2.6 문제풀이

---
+++ python_question/day1/Q1.py
@@ -0,0 +1,7 @@
+# 0 이상이고 100 미만 사이에 있는 모든 8의 배수 리스트 multiples_of_8_list를 정의하라.
+multiples_of_8_list = []
+for i in range (8,101,8):
+    multiples_of_8_list.append(i)
+
+print(multiples_of_8_list)
+
---
+++ python_question/day1/Q3.py
@@ -0,0 +1,11 @@
+# x에 16을 대입하여 다음 수식을 계산하라.
+
+
+def sum(x):
+    sum = 0
+    sum += 8*(x**2)
+    sum += 2*x
+    sum += 2
+    print(sum)
+
+sum(16)
